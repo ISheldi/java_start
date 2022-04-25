2b9ca2ff56099ae51f832a3e22d976a1ffb675ea
iren <irensheludchenko@gmail.com>
2022-04-25 13:02:02 +0300

    Lesson 1. I wrote "Hello World" and with a new line "Hello everybody and my friends"

---
+++ Src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package Src.main.java;
+
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello everybody and my friends");
+
+    }
+}
