66292f5ddf6e6b95c3d855f25b62cde75bd842dd
iren <irensheludchenko@gmail.com>
2022-04-25 18:07:30 +0300

    Lesson 4. Concatenation

---
+++ Src/main/java/Lesson4Concatenation.java
@@ -0,0 +1,15 @@
+package Src.main.java;
+
+public class Lesson4Concatenation {
+    public static void main(String [] args){
+
+        int i = 6;
+        int j = 7;
+        int answer = i * j;
+        String number = "-2";
+        String message = "Hello World!";
+        int result = 40; // answer+number
+        String oneMoreAnswer = message +" "+ result;
+        System.out.println(oneMoreAnswer);
+    }
+}
