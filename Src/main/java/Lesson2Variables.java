61cca2ea516d8cf88844ee15657a1af5e089bc9b
iren <irensheludchenko@gmail.com>
2022-04-25 15:33:55 +0300

    Lesson 2. Variables

---
+++ Src/main/java/Lesson2Variables.java
@@ -0,0 +1,14 @@
+package Src.main.java;
+
+public class Lesson2Variables {
+    public static void main(String [] args){
+        String message = "Hello World!\n";
+        String words = "Java forever\n";
+        System.out.println(message+words);
+
+        String message2 = "Hello World!";
+        String words2 = "Java forever";
+        String space = " ";
+        System.out.println(message2+space+words2);
+    }
+}
