bbfd0a502fe4b27dd98106ff95ec57d1f6b30005
iren <irensheludchenko@gmail.com>
2022-04-25 16:56:08 +0300

    Lesson 3. Integer Variables

---
+++ Src/main/java/Lesson3IntegerVariables.java
@@ -0,0 +1,14 @@
+package Src.main.java;
+
+public class Lesson3IntegerVariables {
+    public static void main(String [] args){
+        int i = 42;
+        int j = 3;
+        System.out.println(i+j);
+
+        int i2 = 6;
+        int j2 = 7;
+        double d = 2.5;
+        System.out.println(i2+j2*d);
+    }
+}
