244907f4414aa22f495aec1bb61c308137f2d296
iren <irensheludchenko@gmail.com>
2022-04-25 18:45:49 +0300

    Lesson 5.

---
+++ Src/main/java/Lesson5.java
@@ -0,0 +1,9 @@
+package Src.main.java;
+
+public class Lesson5 {
+    public static void main(String [] args){
+        int hugeNumber = 521124244;
+        int i = 130281061;
+        System.out.println(hugeNumber / i);
+    }
+}
