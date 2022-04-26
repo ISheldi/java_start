b224cdf96357f26f1a5b91f90e22468cdf898c50
iren <irensheludchenko@gmail.com>
2022-04-26 17:50:54 +0300

    Lesson 4. Исправила.

--- Src/main/java/Lesson4Concatenation.java
+++ Src/main/java/Lesson4Concatenation.java
@@ -6,9 +6,9 @@ public class Lesson4Concatenation {
         int i = 6;
         int j = 7;
         int answer = i * j;
-        String number = "-2";
+        int number = -2;
         String message = "Hello World!";
-        int result = 40; // answer+number
+        int result = answer+number;
         String oneMoreAnswer = message +" "+ result;
         System.out.println(oneMoreAnswer);
     }
